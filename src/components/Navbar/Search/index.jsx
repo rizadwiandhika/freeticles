310bdaaca3895d553ef7666380ac8bef7b9183a7
rizadwiandhika <rizadwiandhika@gmail.com>
2021-10-02 07:07:39 +0700

    update: navsearch ring when focus

--- src/components/Navbar/Search/index.jsx
+++ src/components/Navbar/Search/index.jsx
@@ -4,10 +4,10 @@ import { SearchIcon } from '@heroicons/react/outline'
 
 export default function Search(props) {
   return (
-    <div className="h-full pl-2 rounded-3xl overflow-hidden bg-gray-theme">
+    <div className="h-full pl-2 rounded-3xl overflow-hidden bg-gray-theme focus-within:bg-white focus-within:ring-2 focus-within:ring-blue-theme">
       <SearchIcon className="inline-block h-full w-4 " />
       <input
-        className="text-sm h-full border-none focus:border-transparent focus:outline-none bg-gray-theme"
+        className="text-sm h-full border-none bg-transparent focus:ring-transparent focus:border-transparent focus:outline-none"
         value={props.value}
         placeholder="Search on..."
         onChange={props.changed}
