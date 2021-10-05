9060939cbc2eeb397e9fbc32f7cb2c56f7c7af2a
rizadwiandhika <rizadwiandhika@gmail.com>
2021-10-05 08:18:22 +0700

    add: query related articles

--- src/graphql/query.js
+++ src/graphql/query.js
@@ -117,11 +117,75 @@ const GET_ARTICLE_BY_ID = gql`
   }
 `
 
+const GET_POPULAR_TAGS = gql`
+  query GetPopularTags {
+    tagCountView(limit: 10, order_by: { count: desc_nulls_last }) {
+      count
+      tagName
+    }
+  }
+`
+
+// Input bisa ada wildcard '%' atau '_'
+const GET_ARTICLES_RELATED_TO_TAG = gql`
+  query GetArticlesRelatedToTag($tag: String!) {
+    articles(where: { articleTags: { tagName: { _ilike: $tag } } }) {
+      articleId
+      title
+      username
+      articleTags {
+        tagName
+      }
+    }
+  }
+`
+
+// Bisa pake wildcard, NANTI IMPLEMENTASI HARUS PAKE WILDCARD '%'
+const GET_RELATED_ARTICLES = gql`
+  query GetRelatedArticles($keyword: String!, $username: String = "") {
+    articles(
+      where: {
+        title: { _ilike: $keyword }
+        _or: {
+          subtitle: { _ilike: $keyword }
+          _or: { articleTags: { tagName: { _ilike: $keyword } } }
+        }
+      }
+    ) {
+      articleId
+      articleTags {
+        tagName
+      }
+      likes_aggregate {
+        aggregate {
+          count(columns: likeId)
+        }
+      }
+      publishDate
+      readingTime
+      subtitle
+      thumbnail
+      title
+      likes(where: { username: { _eq: $username } }) {
+        likeId
+        username
+      }
+      username
+      bookmarks(where: { username: { _eq: $username } }) {
+        bookmarkId
+        username
+      }
+    }
+  }
+`
+
 export {
   GET_USERS,
   GET_USER_BY_USERNAME,
   GET_USER_BOOKMARKS,
   GET_USER_ARTICLES,
   GET_ARTICLES,
-  GET_ARTICLE_BY_ID
+  GET_ARTICLE_BY_ID,
+  GET_POPULAR_TAGS,
+  GET_RELATED_ARTICLES
 }
